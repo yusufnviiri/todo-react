c171800fcf9ba31d02c30b7004e7e3abc5908fda
yusufnviiri <yusufnviiri@gmail.com>
2022-06-14 23:54:18 +0300

    Create TO-DO list

---
+++ src/functionBased/components/Navbar.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+
+
+const Navbar = () => {
+
+
+    const links = [
+        {
+          id: 1,
+          path: "/",
+          text: "Home",
+        },
+        {
+          id: 2,
+          path: "/about",
+          text: "About",
+        },
+      ]
+      return (
+        <nav className="navBar">
+          <ul>
+            {links.map(link => {
+              return (
+                <li key={link.id}>
+                  <Link to={link.path}>{link.text}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      )
+  }
+  export default Navbar
